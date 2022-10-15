777cc1ea56af3bd3d237e2ebaab9eb7b5df0b045
Harsh_kumar_yadav <ajayharsh869@gmail.com>
2022-10-15 17:41:06 +0530

    Add files via upload

---
+++ TEMPERATURE-CONVERTER WEBSITE/index.js
@@ -0,0 +1,8 @@
+function display(no){
+    var fahernit=(9*no)/5+32;
+    document.getElementById("result").value=fahernit;
+}
+function display2(no){
+    var celcius=(5*(no-32))/9;
+    document.getElementById("result").value=celcius;
+}
